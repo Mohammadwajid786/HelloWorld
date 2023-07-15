80bcb80c23d9fd923e8e55580e7d73bd0e43ad4f
sc <sc@DESKTOP-C12QJ93>
2023-07-15 18:03:59 +0100

    initial commit

---
+++ Git_Project/src/Com/wajid/Test_For_Git.java
@@ -0,0 +1,12 @@
+package Com.wajid;
+
+public class Test_For_Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("pring gitgub code");
+
+	}
+
+}
